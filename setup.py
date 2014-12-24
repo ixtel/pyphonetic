a82cc4f9e168ddd5654cdd485a16438c7475a905
serg <sjuriev@gmail.com>
2014-12-24 11:05:42 +0300

    - Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyphonetic',
+    version='0.1',
+    url='',
+    license='BSD',
+    author='serg',
+    author_email='sjuriev@gmail.com',
+    description='Pure python realisation of Daitch-Mokotoff and Caverphone algorithm with russian cyrillic support',
+    py_modules=['phonetic']
+)
